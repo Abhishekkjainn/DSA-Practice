1a83f74c6cc845cbcc455cc96f30f8d876be6904
abhishekkjainn <jainabhishek1904@gmail.com>
2025-02-14 19:10:48 +0530

    Done Singly linked list

--- linkedlist.java
+++ linkedlist.java
@@ -1,3 +1,4 @@
+//Node datastructure
 class Node {
     int data;
     Node next;
@@ -14,6 +15,8 @@ class Node {
 }
 
 class linkedlist {
+
+    // convert an array to a LL
     public static Node convertarrtoLL(int[] arr) {
         Node head = new Node(arr[0]);
         Node mover = head;
@@ -25,6 +28,7 @@ class linkedlist {
         return head;
     }
 
+    // get the length of LL
     public static int lengthofLL(Node head) {
         int count = 0;
         Node temp = head;
@@ -35,6 +39,7 @@ class linkedlist {
         return count;
     }
 
+    // search in a LL
     public static boolean searchLL(Node head, int target) {
         Node temp = head;
         while (temp != null) {
@@ -47,6 +52,7 @@ class linkedlist {
         return false;
     }
 
+    // Remove the head
     public static Node removeHead(Node head) {
         if (head == null) {
             return head;
@@ -56,6 +62,7 @@ class linkedlist {
         return head;
     }
 
+    // remove tail
     public static Node removeTail(Node head) {
         Node temp = head;
         if (head == null) {
@@ -75,11 +82,113 @@ class linkedlist {
         return head;
     }
 
+    // delete kth element
+    public static Node deletek(Node head, int k) {
+        if (head == null) {
+            return head;
+        }
+        if (k == 1) {
+            // Node temp = head;
+            head = head.next;
+            return head;
+        }
+        int count = 0;
+        Node temp = head;
+        Node prev = null;
+        while (temp != null) {
+            count++;
+            if (count == k) {
+                prev.next = prev.next.next;
+                break;
+            } else {
+                prev = temp;
+                temp = temp.next;
+            }
+        }
+        return head;
+    }
+
+    // delete an element
+    public static Node deleteel(Node head, int k) {
+
+        if (head == null) {
+            return head;
+        }
+        if (head.data == k) {
+            head = head.next;
+            return head;
+        }
+        Node temp = head;
+        Node prev = null;
+        while (temp != null) {
+            if (temp.data == k) {
+                prev.next = prev.next.next;
+                break;
+            } else {
+                prev = temp;
+                temp = temp.next;
+            }
+        }
+        return head;
+    }
+
+    // Insertion
+    public static Node insertheadstart(Node head, int val) {
+        Node temp = new Node(val, head);
+        return temp;
+    }
+
+    public static Node insertheadtail(Node head, int val) {
+        if (head == null) {
+            return new Node(val);
+        }
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        Node tail = new Node(val, null);
+        temp.next = tail;
+        return head;
+    }
+
+    public static Node insertatk(Node head, int k, int val) {
+        if (head == null) {
+            if (k == 1) {
+                Node newnode = new Node(val, head);
+                return newnode;
+            }
+        }
+        if (k == 1) {
+            Node newnode = new Node(val, head);
+            return newnode;
+        }
+
+        int count = 0;
+        Node temp = head;
+        while (temp != null) {
+            count++;
+            if (count == (k - 1)) {
+                Node newnode = new Node(val, temp.next);
+                // newnode.next = temp.next;
+                temp.next = newnode;
+                break;
+            } else {
+                temp = temp.next;
+            }
+        }
+        return head;
+    }
+
     public static void main(String[] args) {
         int[] arr = { 5, 2, 4, 8, 6 };
         Node head = convertarrtoLL(arr);
         // head = removeHead(head);
         // head = removeTail(head);
+        // head = deleteel(head, 4);
+        // head = insertheadstart(head, 20);
+        // head = insertheadtail(head, 25);
+        // head = insertatk(head, 1, 15);
+
         Node temp = head;
 
         while (temp != null) {
@@ -88,6 +197,6 @@ class linkedlist {
         }
 
         System.out.println("length - " + lengthofLL(head));
-        System.out.println(searchLL(head, 5));
+        System.out.println("Present - " + searchLL(head, 5));
     }
 }
